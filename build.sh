2c8646ac0cf240f99ede3b91ca372296c4b4c97b
huki <gk7huki@gmail.com>
2018-05-21 12:24:03 +0530

    initial commit

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+archs=(
+  "linux32"
+  "linux64"
+  "win32"
+  "win64"
+)
+
+prefix=(
+  ""
+  ""
+  "i686-w64-mingw32-"
+  "x86_64-w64-mingw32-"
+)
+
+ccflags=(
+  "-m32"
+  "-m64"
+  "-m32"
+  "-m64"
+)
+
+rm -rf "./obj"
+rm -rf "./lib"
+
+for i in ${!archs[@]}; do
+  mkdir -p "obj/${archs[$i]}"
+  mkdir -p "lib/${archs[$i]}"
+
+  ${prefix[$i]}gcc ${ccflags[$i]} -Iinclude -c src/glad.c -o obj/${archs[$i]}/glad.o
+  ${prefix[$i]}ar crs lib/${archs[$i]}/libglad-static.a obj/${archs[$i]}/glad.o
+  ${prefix[$i]}strip lib/${archs[$i]}/libglad-static.a
+done
