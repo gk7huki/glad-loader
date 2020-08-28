eb56e49f33f343d27c3ea52f98a686c17e317442
huki <gk7huki@gmail.com>
2020-08-28 20:59:13 +0530

    upgrade to glad2-mx

--- build.sh
+++ build.sh
@@ -29,7 +29,9 @@ for i in ${!archs[@]}; do
   mkdir -p "lib/${archs[$i]}"
 
   prefix=`[ ! -z ${hosts[$i]} ] && echo ${hosts[$i]}-`
-  ${prefix}gcc ${ccflags[$i]} -Iinclude -c src/glad.c -o obj/${archs[$i]}/glad.o
-  ${prefix}ar crs lib/${archs[$i]}/libglad-static.a obj/${archs[$i]}/glad.o
+  ${prefix}gcc ${ccflags[$i]} -Iinclude -c src/gl.c -o obj/${archs[$i]}/gl.o
+  ${prefix}gcc ${ccflags[$i]} -Iinclude -c src/egl.c -o obj/${archs[$i]}/egl.o
+  ${prefix}gcc ${ccflags[$i]} -Iinclude -c src/vulkan.c -o obj/${archs[$i]}/vulkan.o
+  ${prefix}ar crs lib/${archs[$i]}/libglad-static.a obj/${archs[$i]}/gl.o obj/${archs[$i]}/egl.o obj/${archs[$i]}/vulkan.o
   ${prefix}strip --strip-unneeded lib/${archs[$i]}/libglad-static.a
 done
