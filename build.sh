d315671963111776de596e8fed31fc6377e05914
Huki <gk7huki@gmail.com>
2022-05-17 18:07:43 +0530

    support single target build

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+arch=${1#--}
+
 archs=(
   "linux32"
   "linux64"
@@ -21,17 +23,15 @@ ccflags=(
   "-m64"
 )
 
-rm -rf "./obj"
-rm -rf "./lib"
-
 for i in ${!archs[@]}; do
-  mkdir -p "obj/${archs[$i]}"
-  mkdir -p "lib/${archs[$i]}"
-
+  if [ ! -z ${arch} ] && [ ! ${archs[$i]} = ${arch} ]; then
+    continue
+  fi
+  mkdir -p "build_${archs[$i]}"
   prefix=`[ ! -z ${hosts[$i]} ] && echo ${hosts[$i]}-`
-  ${prefix}gcc ${ccflags[$i]} -Iinclude -c src/gl.c -o obj/${archs[$i]}/gl.o
-  ${prefix}gcc ${ccflags[$i]} -Iinclude -c src/egl.c -o obj/${archs[$i]}/egl.o
-  ${prefix}gcc ${ccflags[$i]} -Iinclude -c src/vulkan.c -o obj/${archs[$i]}/vulkan.o
-  ${prefix}ar crs lib/${archs[$i]}/libglad-static.a obj/${archs[$i]}/gl.o obj/${archs[$i]}/egl.o obj/${archs[$i]}/vulkan.o
-  ${prefix}strip --strip-unneeded lib/${archs[$i]}/libglad-static.a
+  ${prefix}gcc ${ccflags[$i]} -Iinclude -c src/gl.c -o build_${archs[$i]}/gl.o
+  ${prefix}gcc ${ccflags[$i]} -Iinclude -c src/egl.c -o build_${archs[$i]}/egl.o
+  ${prefix}gcc ${ccflags[$i]} -Iinclude -c src/vulkan.c -o build_${archs[$i]}/vulkan.o
+  ${prefix}ar crs build_${archs[$i]}/libglad-static.a build_${archs[$i]}/gl.o build_${archs[$i]}/egl.o build_${archs[$i]}/vulkan.o
+  ${prefix}strip --strip-unneeded build_${archs[$i]}/libglad-static.a
 done
